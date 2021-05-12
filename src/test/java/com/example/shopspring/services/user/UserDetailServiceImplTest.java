8ec715e71a3a940a9c7488ae3189e7e4b7759e23
Alexey <jamoncheryev1@gmail.com>
2021-05-12 09:30:47 +0300

    Prac № 24

---
+++ src/test/java/com/example/shopspring/services/user/UserDetailServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.example.shopspring.services.user;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class UserDetailServiceImplTest {
+    @Autowired
+    private UserDetailServiceImpl userDetailService;
+
+    @Test
+    void loadUserByUsername() {
+
+    }
+}
